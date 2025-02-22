112b144e9d5d43748c82a18c1712ac5c55545957
Asahi <161689859+6Asahi9@users.noreply.github.com>
2025-02-22 23:06:56 +0530

    Create GridSearch&KFolds.py

---
+++ Section-3/9-ModelSelection/GridSearch&KFolds.py
@@ -0,0 +1,26 @@
+#Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+#Accuracy: 90.33 %
+#Standard Deviation: 6.57 %
+
+#Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [0.25, 0.5, 0.75, 1], 'kernel': ['linear']},
+              {'C': [0.25, 0.5, 0.75, 1], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10,
+                           n_jobs = -1)
+grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+print("Best Parameters:", best_parameters)
+
+#Best Accuracy: 90.67 %
+#Best Parameters: {'C': 0.5, 'gamma': 0.6, 'kernel': 'rbf'}
